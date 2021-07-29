47582ac6845f1624e39cd8bf6b31223acde7e108
Ahmed Kamel <ahmed.kamel@gmail.com>
2021-07-29 00:00:14 -0700

    listen to all routes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.all('/*', (req, res) => {
   const { headers, url, baseUrl, subdomains, hostname, originalUrl, query, path, protocol } = req;
 
   res.send({
